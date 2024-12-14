8f5ccad3b58e41f1fbf0b002767ffab2c7a3af04
Wesley Novais <nwesley1500@gmail.com>
2024-12-14 13:40:29 -0300

    sistema de liberação de crud somente para user logados

--- cars/views.py
+++ cars/views.py
@@ -1,24 +1,9 @@
-from django.shortcuts import render, redirect
 from cars.models import Car
 from cars.forms import CarModelForm
-from django.views import View
-from django.views.generic import ListView, CreateView
-
-
-"""class CarsView(View):
-
-    def get(self, request):
-        cars = Car.objects.all().order_by('model')
-        search = request.GET.get('search')
-
-        if search:
-            cars = Car.objects.filter(model__icontains=search)
-
-        return render(
-            request,
-            'cars.html',
-            {'cars': cars }
-        )"""
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 
 
 class CarsListView(ListView):
@@ -33,22 +18,12 @@ class CarsListView(ListView):
             cars = cars.filter(model__icontains=search)
         return cars
 
+class CarDetailView(DetailView):
+    model = Car
+    template_name = 'car_detail.html'
 
 
-"""class NewCarView(View):
-
-    def get(self, request):
-        new_car_form = CarModelForm()
-        return render(request, 'new_car.html', {'new_car_form': new_car_form })
-
-    def post(self, request):
-        new_car_form = CarModelForm(request.POST, request.FILES)
-        if new_car_form.is_valid():
-            new_car_form.save()
-            return redirect('cars_list')
-        return render(request, 'new_car.html', {'new_car_form': new_car_form })"""
-
-
+@method_decorator(login_required(login_url='/login/'), name='dispatch')
 class NewCarCreateView(CreateView):
     model = Car
     form_class = CarModelForm
@@ -56,30 +31,19 @@ class NewCarCreateView(CreateView):
     success_url = '/cars/'
 
 
+@method_decorator(login_required(login_url='/login/'), name='dispatch')
+class CarUpdateView(UpdateView):
+    model = Car
+    form_class = CarModelForm
+    template_name = 'car_update.html'
+    success_url = '/cars/'
 
-'''
-def cars_view(request):
-    cars = Car.objects.all().order_by('model')
-    search = request.GET.get('search')
+    def get_success_url(self):
+        return reverse_lazy('car_detail', kwargs={'pk': self.object.pk})
 
-    if search:
-        cars = Car.objects.filter(model__icontains=search)
 
-    return render(
-        request,
-        'cars.html',
-        {'cars': cars }
-    )
-'''
-
-'''
-def new_car_view(request):
-    if request.method == 'POST':
-        new_car_form = CarModelForm(request.POST, request.FILES)
-        if new_car_form.is_valid():
-            new_car_form.save()
-            return redirect('cars_list')
-    else:
-        new_car_form = CarModelForm()
-    return render(request, 'new_car.html', {'new_car_form': new_car_form })
-'''
+@method_decorator(login_required(login_url='/login/'), name='dispatch')
+class CarDeleteView(DeleteView):
+    model = Car
+    template_name = 'car_delete.html'
+    success_url = '/cars/'
